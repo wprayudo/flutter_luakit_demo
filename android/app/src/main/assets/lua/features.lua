d42e182b55260e5cad623f4b1488d1bfe9dfdc57
wprayudo <wprayudi@gmail.com>
2019-11-14 07:23:15 +0700

    Update features.lua

--- android/app/src/main/assets/lua/features.lua
+++ android/app/src/main/assets/lua/features.lua
@@ -1,14 +1,4 @@
-json = require 'json'
-csv = require 'csv'
-fio = require 'fio'
-digest = require 'digest'
-Z = require 'luazen'
-client = require 'http.client'
-xml = require 'luarapidxml'
 
-hive.cfg{}
-
-H = require 'hives'
 D = require 'dash'
 S = require 'strings'
 
